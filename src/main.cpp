0a8fe0dd99a904914594db56fe8b7f519a1f2687
Alban-de-tourtier <alban.de-tourtier@epitech.eu>
2022-02-14 14:37:31 +0100

    Add arg and error

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
-#include <iostream>
+/*
+** EPITECH PROJECT, $Year
+** Project
+** File description:
+** Description
+*/
 
-int main()
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int errorArg(std::string arg)
 {
-    std::cout << "Hello, World!" << std::endl;
+    string filecontent; 
+    ifstream input_file(arg);
+
+    if (arg == "-h") {
+        cout << "Enter a filename in param : ./nanotekspice [filename]" << endl;
+        return (0);
+    }
+    if (!input_file.is_open())
+        return (84);
+    filecontent = string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+    cout << filecontent;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return 84;
+    if (errorArg(argv[1]) == 84)
+        return 84;
+    // std::cout << "Hello, World!" << std::endl;
     return 0;
 }
